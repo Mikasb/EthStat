65c4a79765f3edf7602a85cc832613f9cb233462
Mikas <mikas@Mikass-MacBook-Pro.local>
2022-02-09 19:05:36 +0200

    UI changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,22 @@ import { Grid, Card, TextField } from "@mui/material";
 
 function App() {
   const footerHeaderHeight = 60;
+  const OUTER_CONTAINER_PADDING = 10;
+  const INNER_GRID_PADDING = 5;
 
   return (
-    <div className="App">
-      <Grid container>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Grid
+        container
+        sx={{ p: OUTER_CONTAINER_PADDING, maxWidth: 1500, minWidth: 850 }}
+      >
         <Grid
           item
           xs={12}
@@ -21,7 +33,14 @@ function App() {
         >
           THIS IS HEADER
         </Grid>
-        <Grid item xs={12} md={12} sm={12} lg={12}>
+        <Grid
+          item
+          xs={12}
+          md={12}
+          sm={12}
+          lg={12}
+          sx={{ p: INNER_GRID_PADDING }}
+        >
           <TextField
             id="outlined-basic"
             label="Input your MetaMask address..."
@@ -36,56 +55,54 @@ function App() {
           md={12}
           sm={12}
           lg={12}
-          sx={{ ml: 10, mr: 10, mt: 5 }}
+          sx={{ pb: INNER_GRID_PADDING }}
         >
           <Grid item xs={3} md={3} sm={3} lg={3}>
             <CustomDataCard
-              margins={{ mt: 0, mr: 0, mb: 0, ml: 2 }}
+              margins={{ mt: 0, mr: 1, mb: 0, ml: 0 }}
               name="AVERAGE"
               details="Average cost of a transaction"
             />
           </Grid>
           <Grid item xs={3} md={3} sm={3} lg={3}>
             <CustomDataCard
-              margins={{ mt: 0, mr: 0, mb: 0, ml: 2 }}
+              margins={{ mt: 0, mr: 1, mb: 0, ml: 1 }}
               name="MEDIAN"
               details="Median cost of a transaction"
             />
           </Grid>
           <Grid item xs={3} md={3} sm={3} lg={3}>
             <CustomDataCard
-              margins={{ mt: 0, mr: 2, mb: 0, ml: 2 }}
+              margins={{ mt: 0, mr: 1, mb: 0, ml: 1 }}
               name="MAX"
               details="Maximum paid for a transaction"
             />
           </Grid>
           <Grid item xs={3} md={3} sm={3} lg={3}>
             <CustomDataCard
-              margins={{ mt: 0, mr: 0, mb: 0, ml: 0 }}
+              margins={{ mt: 0, mr: 0, mb: 0, ml: 1 }}
               name="TOTAL"
               details="The sum of all transactions paid"
             />
           </Grid>
         </Grid>
 
-        <Grid item xs={6} md={6} sm={6} lg={6}>
-          <Card variant="outlined" sx={{ ml: 10, mt: 5, p: 3 }}>
-            <PieChart />
-          </Card>
+        <Grid container={2} sx={{ pb: INNER_GRID_PADDING }}>
+          <Grid item xs={6} md={6} sm={6} lg={6} sx={{ pr: 3 }}>
+            <Card variant="outlined">
+              <PieChart />
+            </Card>
+          </Grid>
+          <Grid item xs={6} md={6} sm={6} lg={6} sx={{ pl: 3 }}>
+            <Card variant="outlined">
+              <LineChart />
+            </Card>
+            <Card variant="outlined">
+              <LineChart />
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item xs={6} md={6} sm={6} lg={6}>
-          <Card variant="outlined" sx={{ mr: 10, mt: 5, ml: 5, mb: 5, p: 2 }}>
-            <LineChart />
-          </Card>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={12}
-          sm={12}
-          lg={12}
-          sx={{ mr: 10, mt: 5, ml: 10, mb: 5 }}
-        >
+        <Grid item xs={12} md={12} sm={12} lg={12}>
           <DataTable />
         </Grid>
         <Grid
@@ -94,7 +111,7 @@ function App() {
           md={12}
           sm={12}
           lg={12}
-          style={{ height: footerHeaderHeight }}
+          sx={{ height: footerHeaderHeight, pt: 5 }}
         >
           THIS IS FOOTER
         </Grid>
